5f7508ab7262a88fd64564cb8dd9974c57da64b0
ijonghyeon <academy3746@gmail.com>
2023-03-26 01:06:11 +0900

    15. BottomNavigationBar UI (1)

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,10 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tictok_clone/constants/sizes.dart';
+import 'package:tictok_clone/features/main_navigation/widgets/nav_tab.dart';
 
-class MainNavigationScreen extends StatelessWidget {
+class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({Key? key}) : super(key: key);
 
   @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  final screens = [
+    const Center(
+      child: Text("홈"),
+    ),
+    const Center(
+      child: Text("검색"),
+    ),
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(
+            Sizes.size12,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavTab(
+                text: "홈",
+                isSelected: _selectedIndex == 0,
+                icon: FontAwesomeIcons.house,
+                onTap: () => _onTap(0),
+              ),
+              NavTab(
+                text: "검색",
+                isSelected: _selectedIndex == 1,
+                icon: FontAwesomeIcons.magnifyingGlass,
+                onTap: () => _onTap(1),
+              ),
+              NavTab(
+                text: "채팅",
+                isSelected: _selectedIndex == 3,
+                icon: FontAwesomeIcons.message,
+                onTap: () => _onTap(3),
+              ),
+              NavTab(
+                text: "프로필",
+                isSelected: _selectedIndex == 4,
+                icon: FontAwesomeIcons.user,
+                onTap: () => _onTap(4),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
