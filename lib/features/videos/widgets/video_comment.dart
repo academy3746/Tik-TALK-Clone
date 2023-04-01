50757d2fa19ae85201c63a64ef05b82c89798e29
ijonghyeon <academy3746@gmail.com>
2023-04-01 20:05:33 +0900

    25. Modal Bottom Sheet (댓글창) 기능 구현 2차

--- lib/features/videos/widgets/video_comment.dart
+++ lib/features/videos/widgets/video_comment.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tictok_clone/constants/gaps.dart';
 import 'package:tictok_clone/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -38,10 +39,77 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: ListView.builder(
+        body: ListView.separated(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size10,
+            horizontal: Sizes.size16,
+          ),
           itemCount: 10,
-          itemBuilder: (BuildContext context, int index) => Container(
-            child: Text("Hello, everyone!"),
+          itemBuilder: (BuildContext context, int index) => Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CircleAvatar(
+                radius: 18,
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.black,
+                foregroundImage: NetworkImage(
+                  "https://avatars.githubusercontent.com/u/107133642?v=4",
+                ),
+                child: Text("마따크"),
+              ),
+              Gaps.h10,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "마따크",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: Sizes.size14,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                    Gaps.v4,
+                    const Text("강아지가 너무 귀여워요!"),
+                  ],
+                ),
+              ),
+              Gaps.h10,
+              Column(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.heart,
+                    size: Sizes.size20,
+                    color: Colors.grey.shade500,
+                  ),
+                  Gaps.v2,
+                  Text(
+                    "52.2K",
+                    style: TextStyle(
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          separatorBuilder: (BuildContext context, int index) => Gaps.v20,
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Row(
+            children: const [
+              CircleAvatar(
+                radius: 18,
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.black,
+                foregroundImage: NetworkImage(
+                  "https://avatars.githubusercontent.com/u/107133642?v=4",
+                ),
+                child: Text("마따크"),
+              ),
+            ],
           ),
         ),
       ),
