40cbd3f62b6f3439f03c6e1857862b24501d2612
ijonghyeon <academy3746@gmail.com>
2023-04-01 22:26:09 +0900

    28. Modal Bottom Sheet (댓글창) Bug Fix

--- lib/features/videos/widgets/video_comment.dart
+++ lib/features/videos/widgets/video_comment.dart
@@ -12,12 +12,13 @@ class VideoComments extends StatefulWidget {
 
 class _VideoCommentsState extends State<VideoComments> {
   bool _isWriting = false;
+  final ScrollController _scrollController = ScrollController();
 
   void _onClosePressed() {
     Navigator.of(context).pop();
   }
 
-  void _dissmissKeyboard() {
+  void _dismissKeyboard() {
     FocusScope.of(context).unfocus();
     setState(() {
       _isWriting = false;
@@ -58,65 +59,71 @@ class _VideoCommentsState extends State<VideoComments> {
           ],
         ),
         body: GestureDetector(
-          onTap: _dissmissKeyboard,
+          onTap: _dismissKeyboard,
           child: Stack(
             children: [
-              ListView.separated(
-                padding: const EdgeInsets.symmetric(
-                  vertical: Sizes.size10,
-                  horizontal: Sizes.size16,
-                ),
-                itemCount: 10,
-                itemBuilder: (BuildContext context, int index) => Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    CircleAvatar(
-                      radius: 18,
-                      foregroundColor: Colors.white,
-                      backgroundColor: Colors.grey.shade500,
-                      foregroundImage: const NetworkImage(
-                        "https://avatars.githubusercontent.com/u/107133642?v=4",
+              Scrollbar(
+                controller: _scrollController,
+                child: ListView.separated(
+                  controller: _scrollController,
+                  padding: const EdgeInsets.only(
+                    top: Sizes.size10,
+                    bottom: Sizes.size96 + Sizes.size20,
+                    left: Sizes.size16,
+                    right: Sizes.size16,
+                  ),
+                  itemCount: 10,
+                  itemBuilder: (BuildContext context, int index) => Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CircleAvatar(
+                        radius: 18,
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.grey.shade500,
+                        foregroundImage: const NetworkImage(
+                          "https://avatars.githubusercontent.com/u/107133642?v=4",
+                        ),
+                        child: const Text("마따크"),
                       ),
-                      child: const Text("마따크"),
-                    ),
-                    Gaps.h10,
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      Gaps.h10,
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "마따크",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: Sizes.size14,
+                                color: Colors.grey.shade500,
+                              ),
+                            ),
+                            Gaps.v4,
+                            const Text("농ㅋㅋㅋㅋㅋㅋㅋ"),
+                          ],
+                        ),
+                      ),
+                      Gaps.h10,
+                      Column(
                         children: [
+                          FaIcon(
+                            FontAwesomeIcons.heart,
+                            size: Sizes.size20,
+                            color: Colors.grey.shade500,
+                          ),
+                          Gaps.v2,
                           Text(
-                            "마따크",
+                            "52.2K",
                             style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: Sizes.size14,
                               color: Colors.grey.shade500,
                             ),
                           ),
-                          Gaps.v4,
-                          const Text("농ㅋㅋㅋㅋㅋㅋㅋ"),
                         ],
                       ),
-                    ),
-                    Gaps.h10,
-                    Column(
-                      children: [
-                        FaIcon(
-                          FontAwesomeIcons.heart,
-                          size: Sizes.size20,
-                          color: Colors.grey.shade500,
-                        ),
-                        Gaps.v2,
-                        Text(
-                          "52.2K",
-                          style: TextStyle(
-                            color: Colors.grey.shade500,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+                    ],
+                  ),
+                  separatorBuilder: (BuildContext context, int index) => Gaps.v20,
                 ),
-                separatorBuilder: (BuildContext context, int index) => Gaps.v20,
               ),
               // input 박스가 소프트 키보드에 의해 가려진다면 bottomNavigationBar를 사용해서는 안된다..!
               Positioned(
@@ -190,11 +197,12 @@ class _VideoCommentsState extends State<VideoComments> {
                                       if (_isWriting)
                                         GestureDetector(
                                           onTap:
-                                          // Fake Button now...
-                                          _dissmissKeyboard,
+                                              // Fake Button now...
+                                              _dismissKeyboard,
                                           child: FaIcon(
                                             FontAwesomeIcons.circleArrowUp,
-                                            color: Theme.of(context).primaryColor,
+                                            color:
+                                                Theme.of(context).primaryColor,
                                           ),
                                         ),
                                     ],
