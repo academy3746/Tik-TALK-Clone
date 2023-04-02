d7c1b126dcd73770b70f488e56a5289a1f4bf6a3
ijonghyeon <academy3746@gmail.com>
2023-04-02 22:47:03 +0900

    31. 1st Layout of discover_screen.dart

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,13 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:tictok_clone/constants/sizes.dart';
+
+final tabs = [
+  "Timeline",
+  "Users",
+  "Videos",
+  "Music",
+  "LIVE",
+  "Shopping",
+  "Brand",
+];
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("검색"),
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 1,
+          title: const Text("검색"),
+          bottom: TabBar(
+            splashFactory: NoSplash.splashFactory,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size16,
+            ),
+            isScrollable: true,
+            labelStyle: const TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: Sizes.size16,
+            ),
+            indicatorColor: Theme.of(context).primaryColor,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey.shade500,
+            tabs: [
+              for (var tab in tabs)
+                Tab(
+                  text: tab,
+                ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            for (var tab in tabs)
+              Center(
+                child: Text(
+                  tab,
+                  style: const TextStyle(
+                    fontSize: Sizes.size28,
+                  ),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
