ab552d0be2a99d05693f54b161d67c280e0ab7b2
ijonghyeon <academy3746@gmail.com>
2023-04-06 00:05:46 +0900

    33. Let's make our beautiful Gridview (2)

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tictok_clone/constants/gaps.dart';
 import 'package:tictok_clone/constants/sizes.dart';
 
 final tabs = [
@@ -11,68 +14,172 @@ final tabs = [
   "브랜드",
 ];
 
-class DiscoverScreen extends StatelessWidget {
+class DiscoverScreen extends StatefulWidget {
   const DiscoverScreen({Key? key}) : super(key: key);
 
   @override
+  State<DiscoverScreen> createState() => _DiscoverScreenState();
+}
+
+class _DiscoverScreenState extends State<DiscoverScreen> {
+  final TextEditingController _textEditingController = TextEditingController(text: "이곳에 입력하세요.");
+  bool _isWriting = false;
+
+  void _onSearchChanged(String value) {
+    print("Searching for $value");
+  }
+
+  void _onSearchSubmitted(String value) {
+    print("Submitted $value");
+  }
+
+  @override
+  void dispose() {
+    _textEditingController;
+    super.dispose();
+  }
+
+  void _dismissKeyboard() {
+    FocusScope.of(context).unfocus();
+    setState(() {
+      _isWriting = false;
+    });
+  }
+
+  void _onStartWriting() {
+    setState(() {
+      _isWriting = true;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: tabs.length,
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 1,
-          title: const Text("검색"),
-          bottom: TabBar(
-            splashFactory: NoSplash.splashFactory,
-            padding: const EdgeInsets.symmetric(
-              horizontal: Sizes.size16,
+    return GestureDetector(
+      onTap: _dismissKeyboard,
+      child: DefaultTabController(
+        length: tabs.length,
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            elevation: 1,
+            title: CupertinoSearchTextField(
+              onTap: _onStartWriting,
+              controller: _textEditingController,
+              onChanged: _onSearchChanged,
+              onSubmitted: _onSearchSubmitted,
             ),
-            isScrollable: true,
-            labelStyle: const TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: Sizes.size16,
+            bottom: TabBar(
+              splashFactory: NoSplash.splashFactory,
+              padding: const EdgeInsets.symmetric(
+                horizontal: Sizes.size16,
+              ),
+              isScrollable: true,
+              labelStyle: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: Sizes.size16,
+              ),
+              indicatorColor: Theme.of(context).primaryColor,
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey.shade500,
+              tabs: [
+                for (var tab in tabs)
+                  Tab(
+                    text: tab,
+                  ),
+              ],
             ),
-            indicatorColor: Theme.of(context).primaryColor,
-            labelColor: Colors.black,
-            unselectedLabelColor: Colors.grey.shade500,
-            tabs: [
-              for (var tab in tabs)
-                Tab(
-                  text: tab,
+          ),
+          body: TabBarView(
+            children: [
+              GridView.builder(
+                keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+                padding: const EdgeInsets.all(
+                  Sizes.size6,
+                ),
+                itemCount: 20,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: Sizes.size10,
+                  mainAxisSpacing: Sizes.size10,
+                  childAspectRatio: 9 / 20,
+                ),
+                itemBuilder: (BuildContext context, int index) => Column(
+                  children: [
+                    Container(
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(
+                          Sizes.size4,
+                        ),
+                      ),
+                      child: AspectRatio(
+                        aspectRatio: 9 / 16,
+                        child: FadeInImage.assetNetwork(
+                          fit: BoxFit.cover,
+                          placeholder: "assets/images/image006.jpeg",
+                          image:
+                              "https://ac-p1.namu.la/20230405sac/253138772b0da787c9985d2858fca4ac43cb2779dc85564fc5b8a1367dcd5528.png?expires=1680707092&key=iEhIt3GL5GC1GRHDvAf_jA&type=orig",
+                        ),
+                      ),
+                    ),
+                    Gaps.v10,
+                    const Text(
+                      "You mother fucking betrayer must have to die before you go to hell...",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: Sizes.size16 + Sizes.size2,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Gaps.v8,
+                    DefaultTextStyle(
+                      style: TextStyle(
+                        color: Colors.grey.shade600,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      child: Row(
+                        children: [
+                          const CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                "https://avatars.githubusercontent.com/u/107133642?v=4"),
+                            radius: 12,
+                          ),
+                          Gaps.h4,
+                          const Expanded(
+                            child: Text(
+                              "Ozzy Osbourne & Black Sabbath",
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Gaps.h4,
+                          FaIcon(
+                            FontAwesomeIcons.heart,
+                            size: Sizes.size16,
+                            color: Colors.grey.shade600,
+                          ),
+                          Gaps.h2,
+                          const Text(
+                            "2.5M",
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              for (var tab in tabs.skip(1))
+                Center(
+                  child: Text(
+                    tab,
+                    style: const TextStyle(
+                      fontSize: Sizes.size28,
+                    ),
+                  ),
                 ),
             ],
           ),
-        ),
-        body: TabBarView(
-          children: [
-            GridView.builder(
-              padding: const EdgeInsets.all(
-                Sizes.size6,
-              ),
-              itemCount: 20,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: Sizes.size10,
-                mainAxisSpacing: Sizes.size10,
-                childAspectRatio: 9 / 16,
-              ),
-              itemBuilder: (BuildContext context, int index) => Container(
-                color: Colors.amber,
-                child: Center(
-                  child: Text("$index"),
-                ),
-              ),
-            ),
-            for (var tab in tabs.skip(1))
-              Center(
-                child: Text(
-                  tab,
-                  style: const TextStyle(
-                    fontSize: Sizes.size28,
-                  ),
-                ),
-              ),
-          ],
         ),
       ),
     );
