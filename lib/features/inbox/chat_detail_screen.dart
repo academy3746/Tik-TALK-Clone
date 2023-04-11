1924a429fa986d3f62b7cb06e2ade879599834fd
ijonghyeon <academy3746@gmail.com>
2023-04-11 09:13:21 +0900

    43. Chat Detail

--- lib/features/inbox/chat_detail_screen.dart
+++ lib/features/inbox/chat_detail_screen.dart
@@ -11,18 +11,28 @@ class ChatDetailScreen extends StatefulWidget {
 }
 
 class _ChatDetailScreenState extends State<ChatDetailScreen> {
+  final bool isIphone = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: isIphone ? true : false,
         title: ListTile(
           contentPadding: EdgeInsets.zero,
           horizontalTitleGap: Sizes.size10,
-          leading: const CircleAvatar(
-            radius: Sizes.size24,
-            foregroundImage: NetworkImage(
-                "https://i.scdn.co/image/f513995d0080b676e0a198d5ec39bed5744f288c"),
-            child: Text("DIO"),
+          leading: Stack(
+            children: const [
+              Padding(
+                padding: EdgeInsets.all(Sizes.size4),
+                child: CircleAvatar(
+                  radius: Sizes.size24,
+                  foregroundImage: NetworkImage(
+                      "https://i.scdn.co/image/f513995d0080b676e0a198d5ec39bed5744f288c"),
+                  child: Text("DIO"),
+                ),
+              ),
+            ],
           ),
           title: const Text(
             "DIO",
@@ -107,13 +117,51 @@ class _ChatDetailScreenState extends State<ChatDetailScreen> {
               color: Colors.grey.shade50,
               child: Row(
                 children: [
-                  const Expanded(
-                    child: TextField(),
+                  Expanded(
+                    child: TextField(
+                      textInputAction: TextInputAction.send,
+                      keyboardType: TextInputType.emailAddress,
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.symmetric(
+                          horizontal: Sizes.size16,
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: "메세지 보내기",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(
+                            Sizes.size16,
+                          ),
+                          borderSide: BorderSide.none,
+                        ),
+                        suffixIcon: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: const [
+                            FaIcon(
+                              FontAwesomeIcons.faceLaugh,
+                              color: Colors.grey,
+                            ),
+                          ],
+                        ),
+                      ),
+                      cursorColor: Theme.of(context).primaryColor,
+                    ),
                   ),
                   Gaps.h20,
                   Container(
+                    padding: const EdgeInsets.all(
+                      Sizes.size8,
+                    ),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey.shade400,
+                    ),
                     child: const FaIcon(
                       FontAwesomeIcons.paperPlane,
+                      color: Colors.white,
                     ),
                   ),
                 ],
