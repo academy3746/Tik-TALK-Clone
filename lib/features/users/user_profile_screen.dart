449eb02b536a6a0132f22cc2e7a42c1d9b540944
ijonghyeon <academy3746@gmail.com>
2023-04-12 22:01:39 +0900

    45. CustomScrollView 01: How cool is it!

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -10,6 +10,29 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView();
+    return CustomScrollView(
+      slivers: [
+        // Scrollable AppBar
+        SliverAppBar(
+          floating: true,
+          stretch: true,
+          pinned: true,
+          backgroundColor: Theme.of(context).primaryColor,
+          collapsedHeight: 80,
+          expandedHeight: 200,
+          flexibleSpace: FlexibleSpaceBar(
+            stretchModes: const [
+              StretchMode.blurBackground,
+              StretchMode.zoomBackground,
+            ],
+            title: const Text("We Rock!"),
+            background: Image.asset(
+              "assets/images/image007.jpeg",
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
